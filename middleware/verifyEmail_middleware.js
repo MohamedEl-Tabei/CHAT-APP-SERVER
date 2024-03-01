73a747313be925fd2cf8a8506c3f3230e73b0b63
unknown <mohamedeltab33y@gmail.com>
2024-03-01 20:56:11 +0200

    Edit preSendCode middleware

--- middleware/verifyEmail_middleware.js
+++ middleware/verifyEmail_middleware.js
@@ -2,7 +2,7 @@ const Util = require("../base/util");
 const Model = require("../base/model");
 const preSendCodeToVerifyEmail = async (req, res, nxt) => {
   try {
-    let result=await Model.VerifyEmail.exists({email:req.body.email})
+    let result=await Model.VerifyEmail.exists({email:req.body.email.toLowerCase()})
     req.isExist=await result?true:false;
     Util.Validator.Email(req.body.email);
     req.body.email = req.body.email.toLowerCase();
