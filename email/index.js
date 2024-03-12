35ec97b665ae61eb133afb6eaf6f1fe9851f37e0
unknown <mohamedeltab33y@gmail.com>
2024-03-12 23:19:38 +0200

    reset password

--- email/index.js
+++ email/index.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
-//const resetLink = "http://localhost:3000/";
-const resetLink="https://chatapp-izc9.onrender.com/"
-const logo = "https://i.ibb.co/PgSzxkT/image-29.png";
+//const resetLink = "http://localhost:3000/resetPassword";
+const resetLink="https://chatapp-izc9.onrender.com/resetPassword"
+const webLink = "https://chatapp-izc9.onrender.com/";
 const code = async (email, cod) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -38,32 +38,18 @@ const resetPasswordLink = async (email, token) => {
   const html = token.length
     ? ` 
   <div style="text-align:-webkit-center;">
-    <img style="width:150px;background-color:#1c264e;padding:5px;" src=${logo}/>
-    <h2 style="font-weight:100;margin:0px;margin-bottom:15px;margin-top:15px;color:black">Reset your password</h2>
-  </div>
-  <div style="text-align:-webkit-center;color:black">
-    <div style="border:1px solid #e1e4e8;border-radius:10px !important; width:max-content;padding:24px;">
-      <h3 style="margin-bottom:10px;color:black"">Password reset</h3>
-      <div style="text-align:-webkit-left;margin-bottom:8px;color:black"">We heard that you lost your CHATAPP password. Sorry about that!</div>
-      <div style="text-align:-webkit-left;margin-bottom:35px;color:black"">But don’t worry! You can use the following button to reset your password:</div>
-      <a href="${resetLink}resetPassword/${token}" style="text-decoration:none;">
-        <div style="width:max-content;color:white;background-color:#1c264e;border:4px solid #1c264e ;border-radius:10px;padding:10px;font-weight:bold;">Reset your password</div>
-      </a>             
-      <div style="text-align:-webkit-left;margin-top:45px;">Thanks,</div>
-      <div style="text-align:-webkit-left;">Mohamed Eltabei</div>
-    </div>
-  </div>
-  <div style="text-align:-webkit-center;color:#6a737d;margin-top:5px;">
-    <small>You're receiving this email because a password reset was requested for your account.</small>
+    <a href="${resetLink}/${token}" style="text-decoration:none;">
+      <div style="width:max-content;color:white;background-color:#1c264e;border:4px solid #1c264e ;border-radius:10px;padding:10px;font-weight:bold;">Reset your password</div>
+    </a>     
+    <h6 style="margin:0px;margin-top:15px;color:#1c264e">You're receiving this email because a password reset was requested for your account.</h6>
   </div>
 `
     : `
 <div style="text-align:-webkit-center;">
-  <img style="width:150px;" src=${logo}/>
-  <h2 style="font-weight:100;margin:0px;margin-bottom:15px;margin-top:15px;color:red">You don't have a CHATAPP account.</h2>
-</div>
-<div style="text-align:-webkit-center;color:#6a737d;margin-top:5px;">
-  <small>You're receiving this email because a password reset was requested for your account.</small>
+  <a href="${webLink}" style="text-decoration:none;">
+    <div style="width:max-content;color:white;background-color:#1c264e;border:4px solid #1c264e ;border-radius:10px;padding:10px;font-weight:bold;">Create new acount</div>
+  </a>    
+  <h6 style="margin:0px;margin-top:15px;color:red">You don't have a CHATAPP account.</h6>
 </div>
 `;
   const transporter = nodemailer.createTransport({
@@ -79,7 +65,7 @@ const resetPasswordLink = async (email, token) => {
   await transporter.sendMail({
     from: process.env.EMAIL,
     to: email,
-    subject: "Verify Email",
+    subject: "Reset Password",
     html,
   });
 };
