7a9e217802d71e49fa8b3da4952e286f5b9c7dc7
unknown <mohamedeltab33y@gmail.com>
2024-03-20 23:26:18 +0200

    send chat request

--- connection/index.js
+++ connection/index.js
@@ -5,6 +5,23 @@ const onConnection = async (socket, io) => {
     const usr = await Model.User.findByIdAndUpdate(userId, { socketId });
     await io.emit("isOnline", usr._id);
   });
+  await socket.on("sendRequest", async (receiverId, senderId) => {
+    const receiver = await Model.User.findById(receiverId);
+    const sender = await Model.User.findById(senderId);
+    let requestFromYou = [...sender.requestFromYou];
+    let requestToYou = [...receiver.requestToYou];
+    requestFromYou.push(receiver._id);
+    requestToYou.push(sender._id);
+    await sender.updateOne({
+      requestFromYou,
+    });
+    await receiver.updateOne({
+      requestToYou,
+    });
+    await io
+      .to(await receiver.socketId)
+      .emit("requestNotification", await sender._id);
+  });
   await socket.on("disconnect", async () => {
     const user = await Model.User.findOneAndUpdate(
       { socketId },
